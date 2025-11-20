6c6aeaebd6854a7c5541362d70451eab0bd9e057
jesse <jga@JGAs-MacBook-Pro.local>
2025-11-19 23:25:58 -0600

    Initial commit - Titanic ML project

---
+++ titanic.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from ydata_profiling import ProfileReport
+from sklearn import (
+    ensemble,
+    preprocessing,
+    tree,
+)
+from sklearn.metrics import (
+    auc,
+    confusion_matrix,
+    roc_auc_score,
+    roc_curve,
+)
+from sklearn.model_selection import (
+    train_test_split,
+    StratifiedKFold,
+)
+from yellowbrick.classifier import (
+    ConfusionMatrix,
+    ROCAUC,
+)
+from yellowbrick.model_selection import (
+    LearningCurve,
+)
+
+df = pd.read_csv("train.csv")
+orig_df = df.copy()
+
+df.head()
+df.dtypes
